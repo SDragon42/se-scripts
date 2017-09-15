0d24fdcb8259442c7da91c87cd6d15271c4e5431
Nathen Silver <nathen.silver@gmail.com>
2017-09-14 23:19:56 -0500

    Added support for a generic List

--- SharedProject1/Helpers/CollectHelper.cs
+++ SharedProject1/Helpers/CollectHelper.cs
@@ -18,12 +18,19 @@ namespace IngameScript
 {
     static class CollectHelper
     {
-        public static void GetblocksOfTypeWithFirst<T>(IMyGridTerminalSystem gts, List<IMyTerminalBlock> blockList, params Func<IMyTerminalBlock, bool>[] collectMethods) where T : class
+        public static void GetblocksOfTypeWithFirst<T>(IMyGridTerminalSystem gts, List<IMyTerminalBlock> blockList, params Func<IMyTerminalBlock, bool>[] collectMethods) where T : class, IMyTerminalBlock
         {
-            blockList.Clear();
             foreach (var collect in collectMethods)
             {
                 gts.GetBlocksOfType<T>(blockList, collect);
+                if (blockList.Count > 0) return;
+            }
+        }
+        public static void GetblocksOfTypeWithFirst<T>(IMyGridTerminalSystem gts, List<T> blockList, params Func<IMyTerminalBlock, bool>[] collectMethods) where T : class, IMyTerminalBlock
+        {
+            foreach (var collect in collectMethods)
+            {
+                gts.GetBlocksOfType(blockList, collect);
                 if (blockList.Count > 0) return;
             }
         }
