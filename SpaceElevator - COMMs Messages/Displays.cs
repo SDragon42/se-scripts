aeafe1cec543ed684848ebacb5113375d22468bc
Nathen Silver <nathen.silver@gmail.com>
2017-09-29 20:35:20 -0500

    Added the LCD Helper to the Space Elevator scripts

---
+++ SpaceElevator - COMMs Messages/Displays.cs
@@ -0,0 +1,95 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript {
+    static class Displays {
+
+        // ┌┬┐   250c 252c 2510
+        // ├┼┤│  251c 253c 2524 2502
+        // └┴┘   2514 2534 2518
+
+        const string CHR_UP = "↑↑"; //"\u2191\u2191";
+        const string CHR_DOWN = "↓↓"; //"\u2193\u2193";
+        const string CHRS_Carriage_Red = "\uE050\uE03C";
+        const string CHRS_Carriage_Green = "\uE051\uE03D";
+        const string CHRS_Carriage_Blue = "\uE052\uE03E";
+
+
+        public static string BuildAllCarriagePositionSummary(CarriageStatusMessage a1, CarriageStatusMessage a2, CarriageStatusMessage b1, CarriageStatusMessage b2, CarriageStatusMessage maint) {
+            const int max = 14;
+            // Monospace, fontsize 1.0
+
+            var a1Info = GetGraphInfo(a1, max);
+            var a2Info = GetGraphInfo(a2, max);
+            var b1Info = GetGraphInfo(b1, max);
+            var b2Info = GetGraphInfo(b2, max);
+            var maintInfo = GetGraphInfo(maint, max);
+
+            var sb = new StringBuilder();
+            sb.AppendLine(" A2 A1     Maint    B1 B2");
+            sb.AppendLine();
+            sb.AppendLine($" {a2Info.DirText} {a1Info.DirText}      {maintInfo.DirText}      {b1Info.DirText} {b2Info.DirText}");
+            for (var i = max - 1; i >= 0; i--) {
+                var a1Text = (i == a1Info.VertPosNum) ? a1Info.Icon : "  ";
+                var a2Text = (i == a2Info.VertPosNum) ? a2Info.Icon : "  ";
+                var b1Text = (i == b1Info.VertPosNum) ? b1Info.Icon : "  ";
+                var b2Text = (i == b2Info.VertPosNum) ? b2Info.Icon : "  ";
+                var maintText = (i == maintInfo.VertPosNum) ? maintInfo.Icon : "  ";
+
+                if (i == max)
+                    sb.AppendLine($"┌{a2Text}┬{a1Text}┐    ┌{maintText}┐    ┌{b1Text}┬{b2Text}┐");
+                else if (i == 0)
+                    sb.AppendLine($"└{a2Text}┴{a1Text}┘    └{maintText}┘    └{b1Text}┴{b2Text}┘");
+                else
+                    sb.AppendLine($"│{a2Text}│{a1Text}│    │{maintText}│    │{b1Text}│{b2Text}│");
+            }
+
+            return sb.ToString();
+        }
+        static string GetDirectionArrows(CarriageStatusMessage carriage) {
+            return carriage.VerticalSpeed > 0 ? "↑↑" : carriage.VerticalSpeed < 0 ? "↓↓" : "  ";
+        }
+        static string GetCarriageIcon(CarriageStatusMessage carriage) {
+            return carriage.Mode == "Docked" ? CHRS_Carriage_Blue : CHRS_Carriage_Green;
+        }
+        static int GetCarriagePositionIndex(CarriageStatusMessage carriage, int numLines) {
+            var totalDist = carriage.Range2Bottom + carriage.Range2Top;
+            var percent = carriage.Range2Bottom / totalDist;
+            return Convert.ToInt32(Math.Round(numLines * percent, 0));
+        }
+        static CarriageGraphInfo GetGraphInfo(CarriageStatusMessage carriage, int numLines) {
+            if (carriage == null) new CarriageGraphInfo();
+            return new CarriageGraphInfo() {
+                DirText = GetDirectionArrows(carriage),
+                Icon = GetCarriageIcon(carriage),
+                VertPosNum = GetCarriagePositionIndex(carriage, numLines)
+            };
+
+        }
+
+        class CarriageGraphInfo {
+            public CarriageGraphInfo() {
+                DirText = "  ";
+                Icon = "  ";
+                VertPosNum = -1;
+            }
+
+            public string DirText { get; set; }
+            public string Icon { get; set; }
+            public int VertPosNum { get; set; }
+        }
+    }
+}
