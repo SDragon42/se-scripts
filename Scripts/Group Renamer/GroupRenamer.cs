b1d35e2814af52d2bd5dbf25a178cd67c576e709
Nathen Silver <nathen.silver@gmail.com>
2017-11-19 14:39:21 -0600

    Update method signature of Main

--- Scripts/Group Renamer/GroupRenamer.cs
+++ Scripts/Group Renamer/GroupRenamer.cs
@@ -18,7 +18,8 @@ namespace IngameScript {
     partial class Program : MyGridProgram {
         readonly GroupRenamer _renamer = new GroupRenamer();
         readonly List<IMyBlockGroup> _groups = new List<IMyBlockGroup>();
-        public void Main(string argument) {
+
+        public void Main(string argument, UpdateType updateSource) {
             GridTerminalSystem.GetBlockGroups(_groups, _renamer.IsRenameGroup);
             var results = _renamer.RenameAllBlocksInGroups(_groups);
             Echo(results);
