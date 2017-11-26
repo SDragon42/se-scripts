f44101ae2064b5c634af846446a29fd2b816903a
Nathen Silver <nathen.silver@gmail.com>
2017-11-25 21:33:43 -0600

    Added SD Launch System solution framework.

---
+++ Scripts/SD Launch System/Launch Center/01-LaunchCenter-Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VRageMath;
+using VRage.Game;
+using VRage.Collections;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+
+partial class ScriptProgram : MyGridProgram {
+
+
+
+
+
+}
---
+++ Scripts/SD Launch System/Launch Center/02-LaunchCenter-Vars-Constructors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VRageMath;
+using VRage.Game;
+using VRage.Collections;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+
+partial class ScriptProgram {
+
+    public ScriptProgram() {
+        Runtime.UpdateFrequency = UpdateFrequency.Update10;
+    }
+    public void Save() {
+    }
+
+    public void Main(string argument, UpdateType updateSource) {
+    }
+
+}
