2009aed26c1151757a44d89dbaa44ca64f5ae2f0
Nathen Silver <nathen.silver@gmail.com>
2017-12-02 16:43:36 -0600

    Converted to not need a timer block anymore. Renamed Vars. Changed config slightly.

--- Scripts/Space Elevator/SpaceElevator - COMMs Reciever/CommsReciever.cs
+++ Scripts/Space Elevator/SpaceElevator - COMMs Reciever/CommsReciever.cs
@@ -16,51 +16,53 @@ using VRageMath;
 
 namespace IngameScript {
     partial class Program : MyGridProgram {
-        readonly CustomDataConfig _custConfig = new CustomDataConfig();
-        int _configHash = 0;
         const string KEY_ProgramBlockName = "Program Block";
-        const string KEY_TimerBlockName = "Timer Block";
         const string KEY_LogLinesToShow = "Lines to Show";
         const string KEY_LogDisplayName = "Log LCD Name";
 
-        Logging _log;
+        const string DEF_ProgName = "Program - Carriage Control";
+        const string DEF_LogLcdName = "Display - COMM Log";
+        const int DEF_NumLogLines = 20;
 
-        IMyTimerBlock _timer = null;
+        readonly CustomDataConfig _config = new CustomDataConfig();
+        readonly Logging _log = new Logging();
+        readonly Queue<CommMessage> _msgQueue = new Queue<CommMessage>();
+        readonly List<IMyTerminalBlock> _tempBlocks = new List<IMyTerminalBlock>();
+
+        int _configHash = 0;
         IMyProgrammableBlock _targetProgram = null;
         IMyTextPanel _display = null;
 
-        readonly Queue<CommMessage> _messageQueue = new Queue<CommMessage>();
 
         public Program() {
             //Echo = (t) => { }; // Disable Echo
-            _log = new Logging();
+            _config.AddKey(KEY_ProgramBlockName,
+                description: "The is the name of the program block forward messages to.",
+                defaultValue: DEF_ProgName);
+            _config.AddKey(KEY_LogDisplayName,
+                description: "The LCD to display the log on. (OPTIONAL)",
+                defaultValue: DEF_LogLcdName);
+            _config.AddKey(KEY_LogLinesToShow,
+                defaultValue: DEF_NumLogLines.ToString());
 
-            _custConfig.AddKey(KEY_ProgramBlockName,
-                description: "The is the name of the program block forward messages to.");
-            _custConfig.AddKey(KEY_TimerBlockName,
-                description: "The is the name of the timer block that is setup to execute this program.");
-            _custConfig.AddKey(KEY_LogLinesToShow,
-                description: "The number of lines to retain in the display log.",
-                defaultValue: "20");
-            _custConfig.AddKey(KEY_LogDisplayName,
-                description: "(OPTIONAL) The LCD to display the log on.");
 
             ReloadConfig();
+
+            Runtime.UpdateFrequency = UpdateFrequency.Once;
         }
 
         public void Main(string argument, UpdateType updateSource) {
             try {
-                Echo("COMMS Reciever v1.0");
+                Echo("COMMS Reciever v1.1");
                 ReloadConfig();
 
-                _targetProgram = GetBlockWithName<IMyProgrammableBlock>(_custConfig.GetValue(KEY_ProgramBlockName));
-                _timer = GetBlockWithName<IMyTimerBlock>(_custConfig.GetValue(KEY_TimerBlockName));
-                _display = GetBlockWithName<IMyTextPanel>(_custConfig.GetValue(KEY_LogDisplayName));
+                _targetProgram = GetBlockWithName<IMyProgrammableBlock>(_config.GetValue(KEY_ProgramBlockName));
+                _display = GetBlockWithName<IMyTextPanel>(_config.GetValue(KEY_LogDisplayName));
 
                 if (_targetProgram == null) Echo("No target Program Block found.");
-                if (_timer == null) Echo("No timer block found.");
                 if (_display == null) Echo("No Log Display.");
-                if (_targetProgram == null || _timer == null) return;
+
+                if (_targetProgram == null) return;
 
                 ProcessArgument(argument);
                 ProcessQueue();
@@ -69,8 +71,6 @@ namespace IngameScript {
                 Echo(logText);
 
                 if (_display != null) {
-                    _display.Font = LCDFonts.MONOSPACE;
-                    _display.FontSize = 0.6f;
                     _display.ShowPublicTextOnScreen();
                     _display.WritePublicText(logText);
                 }
@@ -78,28 +78,32 @@ namespace IngameScript {
                 Echo(ex.Message);
                 Echo(ex.StackTrace);
                 throw ex;
+            } finally {
+                if (_msgQueue.Count > 0)
+                    Runtime.UpdateFrequency |= UpdateFrequency.Once;
             }
+
         }
 
-        List<IMyTerminalBlock> _blocks = new List<IMyTerminalBlock>();
-        T GetBlockWithName<T>(string blockName) where T : class {
-            GridTerminalSystem.GetBlocksOfType<T>(_blocks, b => IsOnThisGrid(b) && (string.Compare(b.CustomName, blockName, true) == 0));
-            return (_blocks.Count > 0) ? (T)_blocks[0] : null;
+
+        T GetBlockWithName<T>(string name) where T : class {
+            GridTerminalSystem.GetBlocksOfType<T>(_tempBlocks, b => IsOnThisGrid(b) && (string.Compare(b.CustomName, name, true) == 0));
+            return (_tempBlocks.Count > 0) ? (T)_tempBlocks[0] : null;
         }
 
 
         void ReloadConfig() {
             if (_configHash == Me.CustomData.GetHashCode())
                 return;
-            _custConfig.ReadFromCustomData(Me);
-            _custConfig.SaveToCustomData(Me);
+            _config.ReadFromCustomData(Me);
+            _config.SaveToCustomData(Me);
             _configHash = Me.CustomData.GetHashCode();
-            _log.MaxTextLinesToKeep = _custConfig.GetValue(KEY_LogLinesToShow).ToInt(10);
+            _log.MaxTextLinesToKeep = _config.GetValue(KEY_LogLinesToShow).ToInt(DEF_NumLogLines);
         }
 
 
         void ProcessArgument(string argument) {
-            if (string.IsNullOrWhiteSpace(argument)) return;
+            if (argument.Length == 0) return;
             var text = DateTime.Now.ToLongTimeString() + " | ";
             try {
                 CommMessage msg = null;
@@ -112,7 +116,7 @@ namespace IngameScript {
                     return;
                 }
                 text += msg.SenderGridName + " | " + msg.PayloadType;
-                _messageQueue.Enqueue(msg);
+                _msgQueue.Enqueue(msg);
             } finally {
                 _log.AppendLine(text);
             }
@@ -120,21 +124,18 @@ namespace IngameScript {
 
 
         void ProcessQueue() {
-            if (_messageQueue.Count <= 0) return;
+            if (_msgQueue.Count == 0) return;
 
-            var msg = _messageQueue.Dequeue();
+            var msg = _msgQueue.Dequeue();
             if (msg == null) return;
             if (Me.CubeGrid.EntityId == msg.SenderGridEntityId) return;
             if (msg.TargetGridName.Length > 0) {
                 if (string.Compare(msg.TargetGridName, Me.CubeGrid.CustomName, true) != 0) return;
             }
 
-            var arg = msg.ToString();
-            if (!_targetProgram.TryRun(arg)) {
-                _messageQueue.Enqueue(msg);
+            if (!_targetProgram.TryRun(msg.ToString())) {
+                _msgQueue.Enqueue(msg);
             }
-
-            if (_messageQueue.Count > 0) _timer.Trigger();
         }
 
 
