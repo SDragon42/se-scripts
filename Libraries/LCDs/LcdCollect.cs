84e9a3420a2fa95ae6fc390f6808d750575d8e6e
Nathen Silver <nathen.silver@gmail.com>
2017-12-20 21:50:20 -0600

    Renamed LCD collect methods. Added some Corner LCD collect methods that will get both angled and flat lcds.

--- Libraries/LCDs/LcdCollect.cs
+++ Libraries/LCDs/LcdCollect.cs
@@ -18,23 +18,28 @@ namespace IngameScript {
     static partial class Collect {
         public static bool IsTextPanel(IMyTerminalBlock b) => b is IMyTextPanel;
 
-        public static bool IsSmBlockTextPanel(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "SmallTextPanel");
-        public static bool IsLgBlockTextPanel(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "LargeTextPanel");
+        public static bool IsSmTextPanel(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "SmallTextPanel");
+        public static bool IsLgTextPanel(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "LargeTextPanel");
 
-        public static bool IsLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmBlockLcd(b) || IsLgBlockLcd(b)));
-        public static bool IsSmBlockLcd(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "SmallLCDPanel");
-        public static bool IsLgBlockLcd(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "LargeLCDPanel");
+        public static bool IsLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmLcd(b) || IsLgLcd(b)));
+        public static bool IsSmLcd(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "SmallLCDPanel");
+        public static bool IsLgLcd(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "LargeLCDPanel");
 
-        public static bool IsWideLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmBlockWideLcd(b) || IsLgBlockWideLcd(b)));
-        public static bool IsSmBlockWideLcd(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "SmallLCDPanelWide");
-        public static bool IsLgBlockWideLcd(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "LargeLCDPanelWide");
+        public static bool IsWideLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmWideLcd(b) || IsLgWideLcd(b)));
+        public static bool IsSmWideLcd(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "SmallLCDPanelWide");
+        public static bool IsLgWideLcd(IMyTerminalBlock b) => (IsTextPanel(b) && b.BlockDefinition.SubtypeId == "LargeLCDPanelWide");
 
-        public static bool IsCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmBlockCornerLcd(b) || IsLgBlockCornerLcd(b)));
-        public static bool IsSmBlockCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (b.BlockDefinition.SubtypeId == "SmallBlockCorner_LCD_1" || b.BlockDefinition.SubtypeId == "SmallBlockCorner_LCD_2"));
-        public static bool IsLgBlockCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (b.BlockDefinition.SubtypeId == "LargeBlockCorner_LCD_1" || b.BlockDefinition.SubtypeId == "LargeBlockCorner_LCD_2"));
+        public static bool IsAngledCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmAngledCornerLcd(b) || IsLgAngledCornerLcd(b)));
+        public static bool IsSmAngledCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (b.BlockDefinition.SubtypeId == "SmallBlockCorner_LCD_1" || b.BlockDefinition.SubtypeId == "SmallBlockCorner_LCD_2"));
+        public static bool IsLgAngledCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (b.BlockDefinition.SubtypeId == "LargeBlockCorner_LCD_1" || b.BlockDefinition.SubtypeId == "LargeBlockCorner_LCD_2"));
 
-        public static bool IsCornerFlatLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmBlockCornerFlatLcd(b) || IsLgBlockCornerFlatLcd(b)));
-        public static bool IsSmBlockCornerFlatLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (b.BlockDefinition.SubtypeId == "SmallBlockCorner_LCD_Flat_1" || b.BlockDefinition.SubtypeId == "SmallBlockCorner_LCD_Flat_2"));
-        public static bool IsLgBlockCornerFlatLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (b.BlockDefinition.SubtypeId == "LargeBlockCorner_LCD_Flat_1" || b.BlockDefinition.SubtypeId == "LargeBlockCorner_LCD_Flat_2"));
+        public static bool IsFlatCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmFlatCornerLcd(b) || IsLgFlatCornerLcd(b)));
+        public static bool IsSmFlatCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (b.BlockDefinition.SubtypeId == "SmallBlockCorner_LCD_Flat_1" || b.BlockDefinition.SubtypeId == "SmallBlockCorner_LCD_Flat_2"));
+        public static bool IsLgFlatCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (b.BlockDefinition.SubtypeId == "LargeBlockCorner_LCD_Flat_1" || b.BlockDefinition.SubtypeId == "LargeBlockCorner_LCD_Flat_2"));
+
+        public static bool IsCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsAngledCornerLcd(b) || IsFlatCornerLcd(b)));
+        public static bool IsSmCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsSmAngledCornerLcd(b) || IsSmFlatCornerLcd(b)));
+        public static bool IsLgCornerLcd(IMyTerminalBlock b) => (IsTextPanel(b) && (IsLgAngledCornerLcd(b) || IsLgFlatCornerLcd(b)));
+
     }
 }
