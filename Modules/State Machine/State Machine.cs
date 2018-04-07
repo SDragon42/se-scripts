47ba39466c3c650fea221904181b88cc0514c8b4
Nathen Silver <nathen.silver@gmail.com>
2018-04-07 16:18:07 -0500

    Made StateMachine support generics. Added support for a custom "Continue" method.

--- Modules/State Machine/State Machine.cs
+++ Modules/State Machine/State Machine.cs
@@ -16,49 +16,70 @@ using VRageMath;
 
 namespace IngameScript {
     partial class Program {
-        class StateMachine {
+        class StateMachine<T> {
             readonly List<string> Keys2Remove = new List<string>();
-            readonly Dictionary<string, IEnumerator<bool>> AllTasks = new Dictionary<string, IEnumerator<bool>>();
+            readonly Dictionary<string, IEnumerator<T>> AllTasks = new Dictionary<string, IEnumerator<T>>();
+            readonly Func<T, bool> ContinueCheck;
 
-            public bool RunAll() {
-                bool ranOp = false;
-                foreach (var key in AllTasks.Keys) {
-                    var task = AllTasks[key];
-                    if (!task.MoveNext() || !task.Current)
-                        Keys2Remove.Add(key);
-                    else
-                        ranOp = true;
-                }
-
-                while (Keys2Remove.Count > 0) {
-                    var key = Keys2Remove[0];
-                    Keys2Remove.RemoveAt(0);
-                    Remove(key);
-                }
-
-                return ranOp;
+            public StateMachine(Func<T, bool> continueMethod = null) {
+                ContinueCheck = (continueMethod != null)
+                    ? continueMethod
+                    : (r) => false;
             }
 
-            public void Add(IEnumerator<bool> task) => Add(DateTime.Now.ToString(), task);
-            public void Add(string key, IEnumerator<bool> task, bool replace = false) {
-                var hasKey = AllTasks.ContainsKey(key);
+            public void RunAll() {
+                if (!HasTasks) return;
+                foreach (var key in AllTasks.Keys) RunTask(key);
+                RemoveKeys();
+            }
+            public T Run(string key) {
+                if (!HasTask(key)) return default(T);
+                var result = RunTask(key);
+                RemoveKeys();
+                return result;
+            }
+            
+            public void Add(IEnumerator<T> task) => Add(DateTime.Now.ToString(), task);
+            public void Add(string key, IEnumerator<T> task, bool replace = false) {
+                var hasKey = HasTask(key);
                 if (hasKey && !replace) return;
                 if (hasKey && replace) Remove(key);
                 AllTasks.Add(key, task);
             }
             public void Remove(string key) {
-                if (!AllTasks.ContainsKey(key)) return;
+                if (!HasTask(key)) return;
                 var task = AllTasks[key];
                 task.Dispose();
                 AllTasks.Remove(key);
             }
 
-            public void RemoveAll() {
+            public void Clear() {
                 foreach (var p in AllTasks) p.Value.Dispose();
                 AllTasks.Clear();
             }
 
-            public bool HasKey(string key) => AllTasks.ContainsKey(key);
+            public bool HasTask(string key) => AllTasks.ContainsKey(key);
+            public bool HasTasks => AllTasks.Count > 0;
+
+
+
+            private T RunTask(string key) {
+                var task = AllTasks[key];
+                if (!task.MoveNext()) {
+                    Keys2Remove.Add(key);
+                    return default(T);
+                }
+                if (!ContinueCheck(task.Current))
+                    Keys2Remove.Add(key);
+                return task.Current;
+            }
+            private void RemoveKeys() {
+                while (Keys2Remove.Count > 0) {
+                    var key = Keys2Remove[0];
+                    Keys2Remove.RemoveAt(0);
+                    Remove(key);
+                }
+            }
         }
     }
 }
