242324fd82c048b4300e5bbe086318a5aba1f9d2
Nathen Silver <nathen.silver@gmail.com>
2018-04-08 18:10:54 -0500

    StateMachine: Changed to allow adding tasks from tasks. key is now required. RemoveCompleted() added.

--- Modules/State Machine/State Machine.cs
+++ Modules/State Machine/State Machine.cs
@@ -18,6 +18,7 @@ namespace IngameScript {
     partial class Program {
         class StateMachine<T> {
             readonly List<string> Keys2Remove = new List<string>();
+            readonly List<string> Keys = new List<string>();
             readonly Dictionary<string, IEnumerator<T>> AllTasks = new Dictionary<string, IEnumerator<T>>();
             readonly Func<T, bool> ContinueCheck;
 
@@ -29,24 +30,26 @@ namespace IngameScript {
 
             public void RunAll() {
                 if (!HasTasks) return;
-                foreach (var key in AllTasks.Keys) RunTask(key);
-                RemoveKeys();
+                var i = 0;
+                while (i < Keys.Count) {
+                    RunTask(Keys[i++]);
+                }
             }
             public T Run(string key) {
                 if (!HasTask(key)) return default(T);
                 var result = RunTask(key);
-                RemoveKeys();
                 return result;
             }
             
-            public void Add(IEnumerator<T> task) => Add(DateTime.Now.ToString(), task);
             public void Add(string key, IEnumerator<T> task, bool replace = false) {
                 var hasKey = HasTask(key);
                 if (hasKey && !replace) return;
                 if (hasKey && replace) Remove(key);
                 AllTasks.Add(key, task);
+                if (!hasKey) Keys.Add(key);
             }
             public void Remove(string key) {
+                if (Keys.Contains(key)) Keys.Remove(key);
                 if (!HasTask(key)) return;
                 var task = AllTasks[key];
                 task.Dispose();
@@ -56,10 +59,19 @@ namespace IngameScript {
             public void Clear() {
                 foreach (var p in AllTasks) p.Value.Dispose();
                 AllTasks.Clear();
+                Keys.Clear();
             }
 
             public bool HasTask(string key) => AllTasks.ContainsKey(key);
             public bool HasTasks => AllTasks.Count > 0;
+
+            public void RemoveCompleted() {
+                while (Keys2Remove.Count > 0) {
+                    var key = Keys2Remove[0];
+                    Keys2Remove.RemoveAt(0);
+                    Remove(key);
+                }
+            }
 
 
 
@@ -72,13 +84,6 @@ namespace IngameScript {
                 if (!ContinueCheck(task.Current))
                     Keys2Remove.Add(key);
                 return task.Current;
-            }
-            private void RemoveKeys() {
-                while (Keys2Remove.Count > 0) {
-                    var key = Keys2Remove[0];
-                    Keys2Remove.RemoveAt(0);
-                    Remove(key);
-                }
             }
         }
     }
