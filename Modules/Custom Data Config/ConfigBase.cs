e491cc857aba0bc78add572629191419050d0a37
Nathen Silver <nathen.silver@gmail.com>
2018-04-29 20:35:56 -0500

    Removed this from being an extension type class.

--- Modules/Custom Data Config/ConfigBase.cs
+++ Modules/Custom Data Config/ConfigBase.cs
@@ -16,19 +16,20 @@ using VRageMath;
 
 namespace IngameScript
 {
-    abstract class ConfigBase<K>
-    {
-        protected readonly static string[] SepBlankLine = new string[] { "\n\n" };
-        protected readonly static char[] SepNewLine = new char[] { '\n' };
-        protected readonly static char[] SepEquals = new char[] { '=' };
+    partial class Program {
+        abstract class ConfigBase<K> {
+            protected readonly static string[] SepBlankLine = new string[] { "\n\n" };
+            protected readonly static char[] SepNewLine = new char[] { '\n' };
+            protected readonly static char[] SepEquals = new char[] { '=' };
 
-        protected readonly Dictionary<string, K> Items = new Dictionary<string, K>();
+            protected readonly Dictionary<string, K> Items = new Dictionary<string, K>();
 
-        public void Clear() => Items.Clear();
+            public void Clear() => Items.Clear();
 
-        public bool ContainsKey(string key) => Items.ContainsKey(key);
+            public bool ContainsKey(string key) => Items.ContainsKey(key);
 
-        public abstract void Load(IMyTerminalBlock block, bool addIfMissing = false);
-        public abstract void Save(IMyTerminalBlock block);
+            public abstract void Load(IMyTerminalBlock block, bool addIfMissing = false);
+            public abstract void Save(IMyTerminalBlock block);
+        }
     }
 }
