ee10730d9bbfab42f1fc31cb2c32cfbcd336123e
Nathen Silver <nathen.silver@gmail.com>
2018-06-16 20:11:59 -0600

    Removed LINQ expressions

--- Library/GroupRenamer.cs
+++ Library/GroupRenamer.cs
@@ -18,7 +18,10 @@ namespace IngameScript {
     partial class Program {
         class GroupRenamer {
             delegate int RenameMethodSig(List<IMyTerminalBlock> blocks, string content);
+
             readonly Dictionary<string, RenameMethodSig> _renameMethods = new Dictionary<string, RenameMethodSig>();
+            readonly List<IMyTerminalBlock> _groupBlocks = new List<IMyTerminalBlock>();
+
             public GroupRenamer() {
                 _renameMethods.Add("rename to:", RenameMethods.RenameTo);
                 _renameMethods.Add("num rename to:", RenameMethods.NumberRenameTo);
@@ -27,23 +30,21 @@ namespace IngameScript {
                 _renameMethods.Add("suffix with:", RenameMethods.SuffixWith);
                 _renameMethods.Add("remove suffix:", RenameMethods.RemoveSuffix);
             }
-
-            readonly List<IMyTerminalBlock> _groupBlocks = new List<IMyTerminalBlock>();
-
+            
             public bool IsRenameGroup(IMyBlockGroup g) {
-                return _renameMethods
-                    .Where(b => g.Name.StartsWith(b.Key, StringComparison.CurrentCultureIgnoreCase))
-                    .Any();
+                foreach (var dic in _renameMethods)
+                    if (g.Name.StartsWith(dic.Key, StringComparison.InvariantCultureIgnoreCase))
+                        return true;
+                return false;
             }
 
             public string RenameAllBlocksInGroups(List<IMyBlockGroup> _allGroups) {
                 var log = new StringBuilder();
 
-                foreach (var currentGroup in _allGroups.Where(IsRenameGroup)) {
+                foreach (var currentGroup in _allGroups) {
+                    if (!IsRenameGroup(currentGroup)) continue;
                     currentGroup.GetBlocks(_groupBlocks);
-                    var methodKeyPair = _renameMethods
-                        .Where(b => currentGroup.Name.StartsWith(b.Key, StringComparison.CurrentCultureIgnoreCase))
-                        .FirstOrDefault();
+                    var methodKeyPair = GetMethodKeyPair(currentGroup);
                     var content = currentGroup.Name.Substring(methodKeyPair.Key.Length);
                     var count = methodKeyPair.Value.Invoke(_groupBlocks, content);
                     log.AppendLine(currentGroup.Name);
@@ -51,6 +52,14 @@ namespace IngameScript {
                 }
 
                 return log.ToString();
+            }
+
+            KeyValuePair<string, RenameMethodSig> GetMethodKeyPair(IMyBlockGroup currentGroup) {
+                foreach (var kp in _renameMethods) {
+                    if (currentGroup.Name.StartsWith(kp.Key, StringComparison.CurrentCultureIgnoreCase))
+                        return kp;
+                }
+                return new KeyValuePair<string, RenameMethodSig>("", (l, c) => 0);
             }
 
         }
