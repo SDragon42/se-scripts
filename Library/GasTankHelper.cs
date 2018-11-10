822a0b40dd43e77fc166ae22344398e32356be7a
SDragon42 <nathen.silver@gmail.com>
2018-11-10 10:22:12 -0700

    (TESTING) Simplified getting the filled % from IMyGasTank.

--- Library/GasTankHelper.cs
+++ Library/GasTankHelper.cs
@@ -33,5 +33,11 @@ namespace IngameScript {
             public static double GetTanksFillPercentage(List<IMyGasTank> tankList) => tankList.Average(t => t.FilledRatio);
             
         }
+
+        static class GasTankHelper_TEST {
+            public static double GetTanksFillPercentage(List<IMyTerminalBlock> tankList) => GetTanksFillPercentage(tankList.Where(t => t is IMyGasTank).Cast<IMyGasTank>());
+            public static double GetTanksFillPercentage(IEnumerable<IMyGasTank> tankList) => tankList.Average(t => t.FilledRatio);
+
+        }
     }
 }
