cb48d835732c37016a73770f1aa4e20fecbe6d15
SDragon42 <nathen.silver@gmail.com>
2018-11-15 22:33:03 -0700

    Updated to the welder factory script

--- Malhavoc - WelderFactory Init/Program.cs
+++ Malhavoc - WelderFactory Init/Program.cs
@@ -18,57 +18,39 @@ using VRageMath;
 namespace IngameScript {
     partial class Program : MyGridProgram {
 
-        const string GROUP_ALL_PISTON = "Factory Piston Toggle";
-        const string GROUP_WELDERS = "Welder Group";
-        const float SPEED_OPERATION = 0.015F;
-        const float SPEED_MOVE_TO_POSITION = 0.5F;
+        string GroupKey_AllWelders = string.Empty;
+        string GroupKey_AllPistons = string.Empty;
+        float Speed_Operation = 0.0F;
+        float Speed_MoveToPosition = 0.0F;
 
 
         readonly RunningSymbol Running = new RunningSymbol();
         readonly List<IMyPistonBase> PistonList = new List<IMyPistonBase>();
-        //readonly List<IMyShipWelder> WelderList = new List<IMyShipWelder>();
+        readonly List<IMyShipWelder> WelderList = new List<IMyShipWelder>();
         readonly StateMachine<bool> Operation = new StateMachine<bool>();
 
         string OperationMessage = string.Empty;
 
 
-        Action<string> Debug = (message) => { };
-
-        public Program() {
-            //Debug = Echo;
-        }
-
-
         public void Main(string argument, UpdateType updateSource) {
-            Debug($"Update Rate: {Runtime.UpdateFrequency}");
+            LoadConfig();
             var autoRun = Runtime.UpdateFrequency.HasFlag(UpdateFrequency.Update10);
             if (autoRun)
                 Echo("Running " + Running.GetSymbol(Runtime));
+
             argument = argument?.ToLower();
             switch (argument) {
                 case "extend":
                     Operation.Clear();
-                    Operation.Add("piston", MoveToStart(), true);
+                    Operation.Add("piston", SetFactoryState(true), true);
                     break;
                 case "retract":
                     Operation.Clear();
-                    Operation.Add("piston", ResetToEnd(), true);
+                    Operation.Add("piston", SetFactoryState(false), true);
                     break;
-                    //case "info":
-                    //    LoadBlocks();
-                    //    var welder = WelderList.Where(b => b.CustomName == "Welder - Test")?.FirstOrDefault();
-                    //    if (welder != null) {
-                    //        welder.Enabled = !welder.Enabled;
-                    //        Debug($"Welder: {welder.CustomName} | {welder.Enabled}");
-                    //    }
-                    //    Debug($"# Pistons: {PistonList.Count:N0}");
-                    //    Debug($"# Welders: {WelderList.Count:N0}");
-                    //    break;
             }
 
             Operation.RunAll();
-
-            Debug($"Has Tasks: {Operation.HasTasks}");
 
             if (Operation.HasTasks)
                 Runtime.UpdateFrequency = UpdateFrequency.Update10;
@@ -80,75 +62,140 @@ namespace IngameScript {
             Echo(OperationMessage);
         }
 
-        IEnumerator<bool> MoveToStart() {
-            OperationMessage = "Moving welders to start position";
+        //IEnumerator<bool> MoveToStart() {
+        //    OperationMessage = "Moving welders to start position";
+        //    LoadBlocks();
+        //    yield return true;
+
+        //    //WelderList.ForEach(w => w.Enabled = false);
+        //    //yield return true;
+
+        //    PistonList.ForEach(p => p.Velocity = Speed_MoveToPosition);
+        //    PistonList.ForEach(p => p.Extend());
+
+        //    var allExtended = false;
+        //    do {
+        //        allExtended = PistonList.All(IsExtended);
+        //        yield return true;
+        //    } while (!allExtended);
+
+        //    PistonList.ForEach(p => p.Velocity = Speed_Operation);
+        //    yield return true;
+        //    OperationMessage = "Extended";
+        //    yield return false;
+        //}
+
+        //IEnumerator<bool> ResetToEnd() {
+        //    OperationMessage = "Moving welders to retracted position";
+        //    LoadBlocks();
+        //    yield return true;
+
+        //    //WelderList.ForEach(w => w.Enabled = false);
+        //    //yield return true;
+
+        //    PistonList.ForEach(p => p.Velocity = Speed_MoveToPosition);
+        //    PistonList.ForEach(p => p.Retract());
+
+        //    var allReteacted = false;
+        //    do {
+        //        yield return true;
+        //        allReteacted = PistonList.All(IsReteacted);
+        //    } while (!allReteacted);
+
+        //    PistonList.ForEach(p => p.Velocity = Speed_Operation * -1.0F);
+        //    yield return true;
+        //    OperationMessage = "Retracted";
+        //    yield return false;
+        //}
+
+        IEnumerator<bool> SetFactoryState(bool extend) {
+            OperationMessage = extend
+                ? "Moving welders to start position"
+                : "Moving welders to retracted position";
+
+            Action<IMyPistonBase> MovePistonAction;
+            Func<IMyPistonBase, bool> PositionCheckFunc;
+
+            if (extend) {
+                OperationMessage = "Moving welders to start position";
+                MovePistonAction = (p) => p.Extend();
+                PositionCheckFunc = IsExtended;
+            } else {
+                OperationMessage = "Moving welders to retracted position";
+                MovePistonAction = (p) => p.Retract();
+                PositionCheckFunc = IsReteacted;
+            }
+
             LoadBlocks();
             yield return true;
 
-            //WelderList.ForEach(w => w.Enabled = false);
-            //yield return true;
-
-            PistonList.ForEach(p => p.Velocity = SPEED_MOVE_TO_POSITION);
-            PistonList.ForEach(p => p.Extend());
-
-            var allExtended = false;
-            do {
-                allExtended = PistonList.All(IsExtended);
-                yield return true;
-                //ShowStates("All Extended", allExtended);
-                //WelderList.ForEach(w => w.Enabled = !w.Enabled);
-            } while (!allExtended);
-
-            PistonList.ForEach(p => p.Velocity = SPEED_OPERATION);
-            yield return true;
-            OperationMessage = "Extended";
-            yield return false;
-        }
-
-        IEnumerator<bool> ResetToEnd() {
-            OperationMessage = "Moving welders to retracted position";
-            LoadBlocks();
+            WelderList.ForEach(w => w.Enabled = false);
             yield return true;
 
-            //WelderList.ForEach(w => w.Enabled = false);
-            //yield return true;
+            PistonList.ForEach(p => p.Velocity = Speed_MoveToPosition);
+            PistonList.ForEach(MovePistonAction);
 
-            PistonList.ForEach(p => p.Velocity = SPEED_MOVE_TO_POSITION);
-            PistonList.ForEach(p => p.Retract());
-
-            var allReteacted = false;
+            var allAtEnd = false;
             do {
                 yield return true;
-                allReteacted = PistonList.All(IsReteacted);
-                //ShowStates("All Retracted", allReteacted);
-            } while (!allReteacted);
+                allAtEnd = PistonList.All(PositionCheckFunc);
+            } while (!allAtEnd);
 
-            PistonList.ForEach(p => p.Velocity = SPEED_OPERATION * -1.0F);
+            PistonList.ForEach(p => p.Velocity = Speed_Operation);
+            PistonList.ForEach(MovePistonAction);
             yield return true;
-            OperationMessage = "Retracted";
+            OperationMessage = string.Empty;
             yield return false;
         }
 
 
         void LoadBlocks() {
-            PistonList.Clear();
-            var group = GridTerminalSystem.GetBlockGroupWithName(GROUP_ALL_PISTON);
-            group?.GetBlocksOfType(PistonList);
+            //PistonList.Clear();
+            //var group = GridTerminalSystem.GetBlockGroupWithName(GROUP_ALL_PISTON);
+            //group?.GetBlocksOfType(PistonList);
 
-            //WelderList.Clear();
-            //group = GridTerminalSystem.GetBlockGroupWithName(GROUP_WELDERS);
-            //group?.GetBlocksOfType(WelderList);
+            LoadList(GroupKey_AllPistons, PistonList);
+            LoadList(GroupKey_AllWelders, WelderList);
+        }
+        void LoadList<T>(string groupName, List<T> blockList) where T : class {
+            blockList.Clear();
+            if (string.IsNullOrWhiteSpace(groupName)) return;
+            var group = GridTerminalSystem.GetBlockGroupWithName(groupName);
+            group.GetBlocksOfType(blockList);
         }
 
         bool IsExtended(IMyPistonBase piston) => Math.Round(piston.CurrentPosition, 3) >= Math.Round(piston.MaxLimit, 3);
         bool IsReteacted(IMyPistonBase piston) => Math.Round(piston.CurrentPosition, 3) <= Math.Round(piston.MinLimit, 3);
 
-        //void ShowStates(string msg, bool state) {
-        //    Debug($"{msg}: {state}");
-        //    Debug($"Piston Pos: {PistonList.First().CurrentPosition}");
-        //    Debug($"Piston MIN: {PistonList.First().MinLimit}");
-        //    Debug($"Piston MAX: {PistonList.First().MaxLimit}");
-        //    Debug($"Welder state: {WelderList.First().Enabled}");
-        //}
+
+        const string SECTION_TAG = "Groups";
+        readonly MyIniKey Key_AllWelders = new MyIniKey(SECTION_TAG, "Group - All Welders");
+        readonly MyIniKey Key_AllPistons = new MyIniKey(SECTION_TAG, "Group - All Pistons");
+        readonly MyIniKey Key_SpeedOperation = new MyIniKey(SECTION_TAG, "Pistons - Operation Speed");
+        readonly MyIniKey Key_SpeedPosition = new MyIniKey(SECTION_TAG, "Pistons - Position Speed");
+
+        int _configHashCode = 0;
+        void LoadConfig() {
+            var tmpHashCode = Me.CustomData.GetHashCode();
+            if (_configHashCode == tmpHashCode) return;
+            _configHashCode = tmpHashCode;
+
+            var ini = new MyIni();
+
+            MyIniParseResult result;
+            ini.TryParse(Me.CustomData, out result);
+
+            ini.Add(Key_AllWelders, "");
+            ini.Add(Key_AllPistons, "");
+            ini.Add(Key_SpeedOperation, 0.015f);
+            ini.Add(Key_SpeedPosition, 1.0f);
+
+            GroupKey_AllWelders = ini.Get(Key_AllWelders).ToString();
+            GroupKey_AllPistons = ini.Get(Key_AllPistons).ToString();
+            Speed_Operation = ini.Get(Key_SpeedOperation).ToSingle();
+            Speed_MoveToPosition = ini.Get(Key_SpeedPosition).ToSingle();
+        }
+
+
     }
 }
