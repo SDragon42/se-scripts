1c00edb60122ec8c31dd97f6ce7d65ec1e6708c6
SDragon42 <nathen.silver@gmail.com>
2018-11-22 11:00:06 -0700

    Added 2 new projects

---
+++ Library/LiftCapacity.cs
@@ -0,0 +1,70 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript {
+    partial class Program {
+        public static class LiftCapacity {
+
+            public static double GetMaxMass1(IMyShipController sc, IList<IMyThrust> thrusters, double minTwr, int inventoryMultiplier) {
+                var gravVec = sc.GetNaturalGravity();
+                //gravity in m/s^2
+                var gravMS2 = Math.Sqrt(
+                    Math.Pow(gravVec.X, 2) +
+                    Math.Pow(gravVec.Y, 2) +
+                    Math.Pow(gravVec.Z, 2));
+                var inNaturalGravity = (gravMS2 > 0.0);
+                if (!inNaturalGravity) return 0.0;
+
+
+                var totalMass = sc.CalculateShipMass().TotalMass;
+                // mass of the grid without cargo
+                var baseMass = sc.CalculateShipMass().BaseMass;
+                var cargoMass = totalMass - baseMass;
+                // the mass the game uses for physics calculation
+                var actualMass = baseMass + (cargoMass / inventoryMultiplier);
+
+                var gravityForceOnShip = (actualMass - cargoMass);
+
+                var maxEffectiveThrust = thrusters.Sum(t => t.MaxEffectiveThrust);
+
+
+                // maxMass = (thrust / TWR) * MassNewtons
+
+                return 0.0;
+            }
+
+            public static double? GetMaxMass2(IMyShipController sc, IList<IMyThrust> thrusters, double minTwr, int inventoryMultiplier) {
+                var gravVec = sc.GetNaturalGravity();
+                //gravity in m/s^2
+                var gravMS2 = Math.Sqrt(
+                    Math.Pow(gravVec.X, 2) +
+                    Math.Pow(gravVec.Y, 2) +
+                    Math.Pow(gravVec.Z, 2));
+                var inNaturalGravity = (gravMS2 > 0.0);
+                if (!inNaturalGravity) return null;
+
+
+                var gridMass = sc.CalculateShipMass().BaseMass;
+                var maxEffectiveThrust = thrusters.Sum(t => t.MaxEffectiveThrust);
+
+                var hoverOverridePower = gridMass / maxEffectiveThrust; // TWR = 1.0
+
+                return null;
+            }
+        }
+    }
+}
---
+++ TWR Calculator 2/Program.cs
@@ -0,0 +1,69 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript {
+    partial class Program : MyGridProgram {
+
+        readonly BlocksByOrientation _orientation = new BlocksByOrientation();
+
+        readonly List<IMyThrust> _thrusters = new List<IMyThrust>();
+
+        public Program() {
+        }
+
+        public void Save() {
+        }
+
+        float MinimumTWR = 0;
+        int InventoryMultiplier = 0;
+
+        public void Main(string argument, UpdateType updateSource) {
+            LoadConfig();
+        }
+
+
+        readonly MyIniKey KEY_MinimumTWR = new MyIniKey("Section", "Minimum TWR");
+        readonly MyIniKey KEY_WorldInvMulti = new MyIniKey("Section", "Inventory Multiplier");
+        readonly MyIniKey KEY_Tag = new MyIniKey("Section", "Block Tag");
+        int _lastConfigHash = 0;
+        void LoadConfig() {
+            var configHash = Me.CustomData.GetHashCode();
+            if (configHash == _lastConfigHash)
+                return;
+
+            var ini = new MyIni();
+            ini.TryParse(Me.CustomData);
+
+            ini.Add(KEY_MinimumTWR, 1.5, comment: "The minimum TWR to use for calc maximum cargo capacity.");
+            ini.Add(KEY_WorldInvMulti, 0, comment: "The World setting for Inventory Multiplier");
+
+            MinimumTWR = ini.Get(KEY_MinimumTWR).ToSingle();
+            InventoryMultiplier = ini.Get(KEY_WorldInvMulti).ToInt32();
+
+            if (InventoryMultiplier <= 0) {
+                var b = GridTerminalSystem.GetBlockOfTypeWithFirst<IMyCargoContainer>(Collect.IsCargoContainer);
+                if (b != null) {
+                    InventoryMultiplier = CargoHelper.GetInventoryMultiplier(b);
+                    ini.Set(KEY_WorldInvMulti, InventoryMultiplier);
+                }
+            }
+
+            Me.CustomData = ini.ToString();
+            _lastConfigHash = Me.CustomData.GetHashCode();
+        }
+    }
+}
