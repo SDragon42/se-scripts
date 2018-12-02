226166e19f581c240d6a8884a214fcfa522aa461
SDragon42 <nathen.silver@gmail.com>
2018-12-02 02:14:31 -0700

    Test program

--- TWR Calculator 2/Program.cs
+++ TWR Calculator 2/Program.cs
@@ -18,9 +18,14 @@ using VRageMath;
 namespace IngameScript {
     partial class Program : MyGridProgram {
 
-        readonly BlocksByOrientation _orientation = new BlocksByOrientation();
+        // Modules
+        readonly BlocksByOrientation BlockOrientation = new BlocksByOrientation();
 
-        readonly List<IMyThrust> _thrusters = new List<IMyThrust>();
+
+        // Block Lists
+        readonly List<IMyThrust> LiftThrusters = new List<IMyThrust>();
+        IMyShipController _sc;
+
 
         public Program() {
         }
@@ -28,22 +33,36 @@ namespace IngameScript {
         public void Save() {
         }
 
+
+        // Vars
         float MinimumTWR = 0;
         int InventoryMultiplier = 0;
 
         public void Main(string argument, UpdateType updateSource) {
+            //LiftCapacity.Debug = Echo;
             LoadConfig();
+            LoadBlocks();
+            if (_sc == null) return;
+            if (InventoryMultiplier <= 0) {
+                Echo("ERROR: Inventory Multiplier is not set!");
+                return;
+            }
+            var x = LiftCapacity.GetMaxMass(_sc, LiftThrusters, MinimumTWR, InventoryMultiplier);
+            Echo($"Max Mass: {x:N2} kg");
+
+            var x2 = LiftCapacity.GetMaxMass(_sc, LiftThrusters, 1.0, InventoryMultiplier);
+            Echo($"Max Mass: {x2:N2} kg");
         }
 
 
+        // Configuration
         readonly MyIniKey KEY_MinimumTWR = new MyIniKey("Section", "Minimum TWR");
         readonly MyIniKey KEY_WorldInvMulti = new MyIniKey("Section", "Inventory Multiplier");
         readonly MyIniKey KEY_Tag = new MyIniKey("Section", "Block Tag");
         int _lastConfigHash = 0;
         void LoadConfig() {
             var configHash = Me.CustomData.GetHashCode();
-            if (configHash == _lastConfigHash)
-                return;
+            if (configHash == _lastConfigHash) return;
 
             var ini = new MyIni();
             ini.TryParse(Me.CustomData);
@@ -64,6 +83,28 @@ namespace IngameScript {
 
             Me.CustomData = ini.ToString();
             _lastConfigHash = Me.CustomData.GetHashCode();
+        }
+
+        void LoadBlocks() {
+            _sc = GridTerminalSystem.GetBlockOfTypeWithFirst<IMyShipController>(
+                //b => IsOnThisGrid(b) && Collect.IsTagged(b, ""),
+                b => IsOnThisGrid(b) && b is IMyCockpit && ((IMyCockpit)b).IsMainCockpit,
+                b => IsOnThisGrid(b) && b is IMyRemoteControl && ((IMyRemoteControl)b).IsMainCockpit,
+                b => IsOnThisGrid(b) && b is IMyCockpit,
+                b => IsOnThisGrid(b) && b is IMyRemoteControl);
+
+            LiftThrusters.Clear();
+
+            if (_sc == null) {
+                Echo($"No Cockpit or RemoteControl found.");
+                return;
+            }
+
+            Echo($"SC: {_sc.CustomName}");
+
+            BlockOrientation.Init(_sc);
+            GridTerminalSystem.GetBlocksOfType(LiftThrusters, BlockOrientation.IsDown);
+            Echo($"# Thrusters: {LiftThrusters.Count}");
         }
     }
 }
