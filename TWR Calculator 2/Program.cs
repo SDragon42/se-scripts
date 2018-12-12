cbd593ff1d10e1626020a0a47f35a6086860b089
SDragon42 <nathen.silver@gmail.com>
2018-12-11 22:23:15 -0700

    Added an option to only use the Up thrusters in a group.

--- TWR Calculator 2/Program.cs
+++ TWR Calculator 2/Program.cs
@@ -21,13 +21,20 @@ namespace IngameScript {
         // Modules
         readonly BlocksByOrientation BlockOrientation = new BlocksByOrientation();
 
+        readonly MyIni Ini = new MyIni();
+
 
         // Block Lists
         readonly List<IMyThrust> LiftThrusters = new List<IMyThrust>();
         IMyShipController _sc;
 
+        Action<string> Debug = (text) => { };
+
 
         public Program() {
+            //Debug = Echo;
+            //LiftCapacity.Debug = Echo;
+            LoadConfig(true);
         }
 
         public void Save() {
@@ -37,9 +44,9 @@ namespace IngameScript {
         // Vars
         float MinimumTWR = 0;
         int InventoryMultiplier = 0;
+        string GroupName = string.Empty;
 
         public void Main(string argument, UpdateType updateSource) {
-            //LiftCapacity.Debug = Echo;
             LoadConfig();
             LoadBlocks();
             if (_sc == null) return;
@@ -48,8 +55,11 @@ namespace IngameScript {
                 return;
             }
             var x = LiftCapacity.GetMaxMass(_sc, LiftThrusters, MinimumTWR, InventoryMultiplier);
+            Echo($"At TWR {MinimumTWR:N1}");
             Echo($"Max Mass: {x:N2} kg");
 
+            Echo("");
+            Echo($"At TWR {1.0:N1}");
             var x2 = LiftCapacity.GetMaxMass(_sc, LiftThrusters, 1.0, InventoryMultiplier);
             Echo($"Max Mass: {x2:N2} kg");
         }
@@ -58,52 +68,57 @@ namespace IngameScript {
         // Configuration
         readonly MyIniKey KEY_MinimumTWR = new MyIniKey("Section", "Minimum TWR");
         readonly MyIniKey KEY_WorldInvMulti = new MyIniKey("Section", "Inventory Multiplier");
-        readonly MyIniKey KEY_Tag = new MyIniKey("Section", "Block Tag");
+        readonly MyIniKey KEY_ThrusterGroup = new MyIniKey("Section", "Thruster Group");
+
         int _lastConfigHash = 0;
-        void LoadConfig() {
+        void LoadConfig(bool force = false) {
             var configHash = Me.CustomData.GetHashCode();
-            if (configHash == _lastConfigHash) return;
+            if (configHash == _lastConfigHash && !force) return;
 
-            var ini = new MyIni();
-            ini.TryParse(Me.CustomData);
+            Ini.TryParse(Me.CustomData);
 
-            ini.Add(KEY_MinimumTWR, 1.5, comment: "The minimum TWR to use for calc maximum cargo capacity.");
-            ini.Add(KEY_WorldInvMulti, 0, comment: "The World setting for Inventory Multiplier");
+            Ini.Add(KEY_MinimumTWR, 1.5, comment: "The minimum TWR to use for calc maximum cargo capacity.");
+            Ini.Add(KEY_WorldInvMulti, 0, comment: "The World setting for Inventory Multiplier");
+            Ini.Add(KEY_ThrusterGroup, "", comment: "The Group name to use for thrusters");
 
-            MinimumTWR = ini.Get(KEY_MinimumTWR).ToSingle();
-            InventoryMultiplier = ini.Get(KEY_WorldInvMulti).ToInt32();
+            MinimumTWR = Ini.Get(KEY_MinimumTWR).ToSingle();
+            InventoryMultiplier = Ini.Get(KEY_WorldInvMulti).ToInt32();
+            GroupName = Ini.Get(KEY_ThrusterGroup).ToString();
 
             if (InventoryMultiplier <= 0) {
                 var b = GridTerminalSystem.GetBlockOfTypeWithFirst<IMyCargoContainer>(Collect.IsCargoContainer);
                 if (b != null) {
                     InventoryMultiplier = CargoHelper.GetInventoryMultiplier(b);
-                    ini.Set(KEY_WorldInvMulti, InventoryMultiplier);
+                    Ini.Set(KEY_WorldInvMulti, InventoryMultiplier);
                 }
             }
 
-            Me.CustomData = ini.ToString();
+            Me.CustomData = Ini.ToString();
             _lastConfigHash = Me.CustomData.GetHashCode();
         }
 
         void LoadBlocks() {
             _sc = GridTerminalSystem.GetBlockOfTypeWithFirst<IMyShipController>(
-                //b => IsOnThisGrid(b) && Collect.IsTagged(b, ""),
                 b => IsOnThisGrid(b) && b is IMyCockpit && ((IMyCockpit)b).IsMainCockpit,
                 b => IsOnThisGrid(b) && b is IMyRemoteControl && ((IMyRemoteControl)b).IsMainCockpit,
                 b => IsOnThisGrid(b) && b is IMyCockpit,
                 b => IsOnThisGrid(b) && b is IMyRemoteControl);
-
-            LiftThrusters.Clear();
-
             if (_sc == null) {
                 Echo($"No Cockpit or RemoteControl found.");
                 return;
             }
-
             Echo($"SC: {_sc.CustomName}");
 
             BlockOrientation.Init(_sc);
-            GridTerminalSystem.GetBlocksOfType(LiftThrusters, BlockOrientation.IsDown);
+
+            Func<IMyTerminalBlock, bool> collect;
+            LiftThrusters.Clear();
+            if (!string.IsNullOrWhiteSpace(GroupName)) {
+                var bg = GridTerminalSystem.GetBlockGroupWithName(GroupName);
+                bg?.GetBlocksOfType(LiftThrusters, BlockOrientation.IsDown);
+            }
+            if (LiftThrusters.Count == 0)
+                GridTerminalSystem.GetBlocksOfType(LiftThrusters, BlockOrientation.IsDown);
             Echo($"# Thrusters: {LiftThrusters.Count}");
         }
     }
