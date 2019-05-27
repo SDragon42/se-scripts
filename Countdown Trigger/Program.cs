89e47f9af15f2695196addc33bfbdc87ff1702c3
SDragon42 <nathen.silver@gmail.com>
2019-05-27 14:41:18 -0600

    Updated Countdown Trigger to support multiple timers/displays

--- Countdown Trigger/Program.cs
+++ Countdown Trigger/Program.cs
@@ -25,22 +25,29 @@ namespace IngameScript {
         readonly StateMachine<bool> Operation = new StateMachine<bool>();
 
         public Program() {
-            Commands.Add("start", Start);
-            Commands.Add("abort", Abort);
-
             LoadConfig();
         }
 
         public void Save() {
         }
 
+        static readonly char[] ArgSplit = new char[] { ' ' };
+
         public void Main(string argument, UpdateType updateSource) {
-            if (argument != string.Empty) argument = argument.ToLower();
+            Echo("Countdown Timer");
             LoadConfig();
 
-            Echo("Countdown Timer");
+            var argumentParts = argument.Split(ArgSplit, 2);
+            if (argumentParts.Length == 2) {
+                var cmd = argumentParts[0].ToLower();
+                var tag = argumentParts[1];
 
-            if (Commands.ContainsKey(argument)) Commands[argument]?.Invoke();
+                switch (cmd) {
+                    case "start": StartUndock(tag); break;
+                    case "abort": Abort(tag); break;
+                    default: break;
+                }
+            }
 
             Operation.RunAll();
 
@@ -50,59 +57,81 @@ namespace IngameScript {
                 Runtime.UpdateFrequency = UpdateFrequency.None;
         }
 
-        void Start() {
-            Operation.Clear();
-
-            var _timer = GetBlock<IMyTimerBlock>(Ini.Get(Key_TimerBlock).ToString());
-            var _display = GetBlock<IMyTextPanel>(Ini.Get(Key_DisplayBlock).ToString());
-
-            if (_timer == null) Echo("No Timer block found");
-            if (_display == null) Echo("No LCD block found");
-            if (_timer == null) return;
-
-            Operation.Add("undock", RunUndockSequence(_timer, _display, Ini.Get(Key_NumSeconds).ToDouble(), Ini.Get(Key_DisplayClearSeconds).ToDouble()), true);
-        }
-        void Abort() {
-            Operation.Clear();
-
-            var _display = GetBlock<IMyTextPanel>(Ini.Get(Key_DisplayBlock).ToString());
-            _display?.WriteText(string.Empty);
+        void Abort(string tag) {
+            if (!Operation.HasTask(tag)) return;
+            Operation.Remove(tag);
+            var displayBlocks = new List<IMyTextPanel>();
+            GridTerminalSystem.GetBlocksOfType(displayBlocks, b => Collect.IsTagged(b, tag));
+            displayBlocks.ForEach(d => d.WriteText(string.Empty));
         }
 
-        IEnumerator<bool> RunUndockSequence(IMyTimerBlock _timer, IMyTextPanel _display, double timeRemaining, double clearDelaySeconds) {
+        void StartUndock(string tag) {
+            if (Operation.HasTask(tag)) return;
+            Operation.Add(tag, RunUndockSequence(tag), true);
+        }
 
-            ShowCountdown(_display, timeRemaining);
-            while (timeRemaining >= 0) {
+        IEnumerator<bool> RunUndockSequence(string tag) {
+            var timerBlocks = new List<IMyTimerBlock>();
+            GridTerminalSystem.GetBlocksOfType(timerBlocks, b => Collect.IsTagged(b, tag));
+
+            var displayBlocks = new List<IMyTextPanel>();
+            GridTerminalSystem.GetBlocksOfType(displayBlocks, b => Collect.IsTagged(b, tag));
+
+            if (timerBlocks.Count == 0) Echo("No Timer block found");
+            if (displayBlocks.Count == 0) Echo("No LCD block found");
+            if (timerBlocks.Count > 0) {
+
+                var timeRemaining = CountdownSeconds;
+                ShowCountdown(displayBlocks, timeRemaining);
+                while (timeRemaining >= 0) {
+                    yield return true;
+                    timeRemaining -= Runtime.TimeSinceLastRun.TotalSeconds;
+                    ShowCountdown(displayBlocks, timeRemaining);
+                }
+
+
                 yield return true;
-                timeRemaining -= Runtime.TimeSinceLastRun.TotalSeconds;
-                ShowCountdown(_display, timeRemaining);
-            }
+                timerBlocks.ForEach(t => t.Trigger());
 
-
-            yield return true;
-            _timer.Trigger();
-
-
-            yield return true;
-            while (clearDelaySeconds >= 0) {
                 yield return true;
-                clearDelaySeconds -= Runtime.TimeSinceLastRun.TotalSeconds;
+                var clearDelaySeconds = DisplayClearSeconds;
+                while (clearDelaySeconds >= 0) {
+                    yield return true;
+                    clearDelaySeconds -= Runtime.TimeSinceLastRun.TotalSeconds;
+                }
+
+                displayBlocks.ForEach(d => d.WriteText(string.Empty));
+                yield return false;
             }
-
-            _display?.WriteText(string.Empty);
-            yield return false;
         }
 
-
-        T GetBlock<T>(string name) where T : class, IMyTerminalBlock {
-            GridTerminalSystem.GetBlocksOfType<T>(TmpBlocks, b => b.CustomName == name);
-            return (TmpBlocks.Count == 1) ? (T)TmpBlocks[0] : default(T);
-        }
-
-        void ShowCountdown(IMyTextSurface _display, double timeRemaining) {
+        void ShowCountdown(List<IMyTextPanel> displays, double timeRemaining) {
             var text = Math.Round(timeRemaining, MidpointRounding.AwayFromZero).ToString("N0");
             Echo("Countdown: " + text);
-            _display?.WriteText(text);
+            displays.ForEach(d => d.WriteText(text));
         }
+
+
+        int _configHashCode = 0;
+
+        readonly MyIniKey Key_NumSeconds = new MyIniKey("Timer", "Countdown Time");
+        readonly MyIniKey Key_DisplayClearSeconds = new MyIniKey("Timer", "Display Clear Time");
+
+        void LoadConfig() {
+            var tmpHashCode = Me.CustomData.GetHashCode();
+            if (_configHashCode == tmpHashCode) return;
+
+            Ini.Clear();
+            Ini.TryParse(Me.CustomData);
+
+            Ini.Add(Key_NumSeconds, 30);
+            Ini.Add(Key_DisplayClearSeconds, 5);
+
+            Me.CustomData = Ini.ToString();
+            _configHashCode = Me.CustomData.GetHashCode();
+        }
+
+        double CountdownSeconds => Ini.Get(Key_NumSeconds).ToDouble();
+        double DisplayClearSeconds => Ini.Get(Key_DisplayClearSeconds).ToDouble();
     }
 }
