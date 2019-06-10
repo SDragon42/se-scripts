db4c27335479f08f0e4c72a2b85bdeff4eb980fa
SDragon42 <nathen.silver@gmail.com>
2019-06-10 12:08:55 -0600

    Ship Dock Secure now uses the newer command dictionary method, and displays instructions.

--- Ship Dock-Secure/1ShipDockSecure.cs
+++ Ship Dock-Secure/1ShipDockSecure.cs
@@ -19,10 +19,6 @@ using VRageMath;
 
 namespace IngameScript {
     partial class Program : MyGridProgram {
-        const string CMD_DOCK = "dock";
-        const string CMD_UNDOCK = "undock";
-        const string CMD_TOGGLE = "toggle-dock";
-
         const double BLOCK_RELOAD_TIME = 10;
 
         readonly RunningSymbol _runSymbol = new RunningSymbol();
@@ -31,19 +27,36 @@ namespace IngameScript {
 
         double _timeLastBlockLoad = BLOCK_RELOAD_TIME * 2;
 
+        readonly IDictionary<string, Action> Commands = new Dictionary<string, Action>();
+        readonly string Instructions;
+
         public Program() {
-            //Echo = (t) => { }; // Disable Echo
+
+            Commands.Add("dock", _dockSecure.Dock);
+            Commands.Add("undock", _dockSecure.UnDock);
+            Commands.Add("toggle-dock", _dockSecure.ToggleDock);
+
+            // Instructions
+            var sb = new StringBuilder();
+            sb.AppendLine("Script Commands");
+            foreach (var c in Commands.Keys) sb.AppendLine(c);
+            Instructions = sb.ToString();
+
             _settings.InitConfig(Me);
+
             Runtime.UpdateFrequency = UpdateFrequency.Update10;
         }
 
         public void Main(string argument, UpdateType updateSource) {
+            if (argument != string.Empty) argument = argument.ToLower();
             _timeLastBlockLoad += Runtime.TimeSinceLastRun.TotalSeconds;
             var timeTilUpdate = MathHelper.Clamp(Math.Truncate(BLOCK_RELOAD_TIME - _timeLastBlockLoad) + 1, 0, BLOCK_RELOAD_TIME);
+
             Echo("Dock-Secure v1.3.4 " + _runSymbol.GetSymbol(Runtime));
             Echo($"Scanning for blocks in {timeTilUpdate:N0} seconds.");
             Echo("");
             Echo("Configure script in 'Custom Data'");
+            Echo(Instructions);
 
             if (argument.Length == 0 && (updateSource & UpdateType.Trigger) > 0) {
                 Echo("Execution via Timer block is no longer needed.");
@@ -58,19 +71,10 @@ namespace IngameScript {
                 _timeLastBlockLoad = 0;
             }
 
-            if (argument.Length > 0) {
-                switch (argument.ToLower()) {
-                    case CMD_DOCK: _dockSecure.Dock(); break;
-                    case CMD_UNDOCK: _dockSecure.UnDock(); break;
-                    case CMD_TOGGLE: _dockSecure.ToggleDock(); break;
-                }
-                return;
-            }
+            if (Commands.ContainsKey(argument))
+                Commands[argument]?.Invoke();
 
-            if ((updateSource & UpdateType.Update10) > 0) {
-                _dockSecure.AutoToggleDock();
-            }
-
+            _dockSecure.AutoToggleDock();
         }
 
     }
