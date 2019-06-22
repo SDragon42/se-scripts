4d39169372033cbbec6dcd0b425afb8fab4573fe
SDragon42 <nathen.silver@gmail.com>
2019-06-22 15:12:06 -0600

    Fixed torp block finding. Added Beacon blocks Added Debug echo lines.

--- Guidance Block Launch Control/Program.cs
+++ Guidance Block Launch Control/Program.cs
@@ -20,10 +20,13 @@ using VRageMath;
 namespace IngameScript {
     partial class Program : MyGridProgram {
 
+        const string ScriptTitle = "SDragons Missile Guidance Launcher";
+
         enum MissileSelectionMode { Random = 0, Closest = 1, Furthest = 2 }
 
         // Blocks
-        readonly List<IMyRadioAntenna> gdblocks = new List<IMyRadioAntenna>();
+        readonly List<IMyRadioAntenna> guidanceBlocks = new List<IMyRadioAntenna>();
+        readonly List<IMyBeacon> beaconBlocks = new List<IMyBeacon>();
         IMyTerminalBlock referenceBlock = null;
 
         // Command vars
@@ -33,20 +36,18 @@ namespace IngameScript {
         readonly Random randomGenerator = new Random();
 
         // Config Settings
-        string referenceBlockName = string.Empty;
+        string referenceTag = "Cockpit";
         string missilePrimaryTag = "Torpedo Payload Guidance";
-        string missileSecondaryTag = "";
+        string missileSecondaryTag = string.Empty;
+        string missileBeaconTag = "Torpedo Payload Beacon";
         MissileSelectionMode selectionMode = MissileSelectionMode.Random;
 
-        // config vars
-        int _configHashCode = 0;
 
-        const string SECTION_MISSILE_GUIDANCE = "Missile Guidance";
-        readonly MyIniKey Key_ReferanceBlock = new MyIniKey(SECTION_MISSILE_GUIDANCE, "Reference Block Name");
-        readonly MyIniKey Key_GuidanceTag = new MyIniKey(SECTION_MISSILE_GUIDANCE, "Guidance Tag");
-        readonly MyIniKey Key_LaunchMode = new MyIniKey(SECTION_MISSILE_GUIDANCE, "Launch Mode");
+        Action<string> Debug = (text) => { };
 
         public Program() {
+            //Debug = Echo;
+
             Commands.Add("lock", LockOnAll);
             Commands.Add("off", TurnOffAll);
             Commands.Add("launch", Launch);
@@ -63,21 +64,24 @@ namespace IngameScript {
             foreach (var c in Commands.Keys) sb.AppendLine(c);
             Instructions = sb.ToString();
 
+            Echo(ScriptTitle);
             Echo(Instructions);
 
             ProcessConfig();
         }
 
         public void Main(string argument, UpdateType updateSource) {
-            ProcessConfig();
-            
-            var cmdParts = argument.ToLower().Split(new char[] { ' ' }, 2);
+            Echo(ScriptTitle);
+            missileSecondaryTag = string.Empty;
 
-            missileSecondaryTag = cmdParts.Length == 2 ? cmdParts[1] : string.Empty;
+            ProcessConfig();
+
+            var cmdParts = argument.ToLower().Split(new char[] { ' ' }, 2);
+            missileSecondaryTag = cmdParts.Length == 2 ? cmdParts[1].ToLower() : string.Empty;
 
             LoadBlocks();
 
-            if (gdblocks.Count == 0) {
+            if (guidanceBlocks.Count == 0) {
                 Echo("No missile guidance blocks found");
                 Echo($"Tag: {missilePrimaryTag}");
             }
@@ -87,7 +91,19 @@ namespace IngameScript {
             Echo(Instructions);
         }
 
+
+        // config vars
+        int _configHashCode = 0;
+        const string SEC_MissileGuidanceTags = "Missile Guidance Tags";
+        readonly MyIniKey Key_ReferanceBlock = new MyIniKey(SEC_MissileGuidanceTags, "Reference Block Tag");
+        readonly MyIniKey Key_GuidanceTag = new MyIniKey(SEC_MissileGuidanceTags, "Guidance Tag");
+        readonly MyIniKey Key_BeaconTag = new MyIniKey(SEC_MissileGuidanceTags, "Beacon Tag");
+
+        const string SEC_MissileLaunch = "Missile Launch";
+        readonly MyIniKey Key_LaunchMode = new MyIniKey(SEC_MissileLaunch, "Launch Mode");
+
         void ProcessConfig() {
+            Debug("ProcessConfig()");
             var tmpHashCode = Me.CustomData.GetHashCode();
             if (_configHashCode == tmpHashCode) return;
             _configHashCode = tmpHashCode;
@@ -95,12 +111,16 @@ namespace IngameScript {
             var ini = new MyIni();
 
             // Create Default Config
-            ini.Add(Key_ReferanceBlock, referenceBlockName);
+            ini.Add(Key_ReferanceBlock, referenceTag);
             ini.Add(Key_GuidanceTag, missilePrimaryTag);
+            ini.Add(Key_BeaconTag, missileBeaconTag);
+
             ini.Add(Key_LaunchMode, (int)selectionMode, "Modes: 0 = Random, 1 = Closest, 2 = Furthest");
 
-            referenceBlockName = ini.Get(Key_ReferanceBlock).ToString();
-            missilePrimaryTag = ini.Get(Key_GuidanceTag).ToString();
+
+            referenceTag = ini.Get(Key_ReferanceBlock).ToString().ToLower();
+            missilePrimaryTag = ini.Get(Key_GuidanceTag).ToString().ToLower();
+            missileBeaconTag = ini.Get(Key_BeaconTag).ToString().ToLower();
 
             var mode = ini.Get(Key_LaunchMode).ToInt32();
             if (Enum.IsDefined(typeof(MissileSelectionMode), mode))
@@ -109,18 +129,28 @@ namespace IngameScript {
 
             Me.CustomData = ini.ToString();
             _configHashCode = Me.CustomData.GetHashCode();
+
+            Debug($"Ref: {referenceTag}");
+            Debug($"GTag: {missilePrimaryTag}");
+            Debug($"BTag: {missileBeaconTag}");
+            Debug($"Smode: {selectionMode}");
         }
 
         void LoadBlocks() {
             referenceBlock = GridTerminalSystem.GetBlockOfTypeWithFirst<IMyShipController>(
-                b => b.IsSameConstructAs(Me) && b.CustomName.Equals(referenceBlockName),
+                b => b.IsSameConstructAs(Me) && b.CustomName.ToLower().Contains(referenceTag),
                 b => b.IsSameConstructAs(Me) && b is IMyCockpit && ((IMyCockpit)b).IsMainCockpit,
                 b => b.IsSameConstructAs(Me) && b is IMyCockpit && ((IMyCockpit)b).IsUnderControl,
                 b => b.IsSameConstructAs(Me) && b is IMyCockpit,
                 b => b.IsSameConstructAs(Me) && b is IMyRemoteControl);
             if (referenceBlock == null) referenceBlock = Me;
+            Debug($"FRef: {referenceBlock.CustomName}");
 
-            GridTerminalSystem.GetBlocksOfType(gdblocks, IsMissleGuidance);
+            GridTerminalSystem.GetBlocksOfType(guidanceBlocks, IsMissleGuidance);
+            Debug($"# Found Torps: {guidanceBlocks.Count}");
+
+            GridTerminalSystem.GetBlocksOfType(beaconBlocks, IsMissleBeacon);
+            Debug($"# Found Torps Beacons: {guidanceBlocks.Count}");
         }
 
         bool IsMissleGuidance(IMyTerminalBlock b) {
@@ -131,58 +161,69 @@ namespace IngameScript {
                 if (!customName.Contains(missileSecondaryTag)) return false;
             return true;
         }
-
+        //
+        bool IsMissleBeacon(IMyTerminalBlock b) {
+            if (!b.IsSameConstructAs(Me)) return false;
+            var customName = b.CustomName.ToLower();
+            if (!customName.Contains(missileBeaconTag)) return false;
+            return true;
+        }
 
 
 
         void LockOnAll() {
-            gdblocks.ForEach(LockOn);
+            guidanceBlocks.ForEach(LockOn);
         }
         void LockOn(IMyRadioAntenna b) {
-            //b.ApplyAction("OnOff_On");
             b.Enabled = true;
             b.ApplyAction("Adn.ActionLockOnTarget");
         }
         void TurnOffAll() {
-            gdblocks.ForEach(b => b.Enabled = false);
-            //gdblocks.ForEach(b => b.ApplyAction("OnOff_Off"));
+            guidanceBlocks.ForEach(b => b.Enabled = false);
+            beaconBlocks.ForEach(b => b.Enabled = false);
         }
         void Launch() {
-            var b = MissileSelectionAction[selectionMode]?.Invoke();
-            if (b == null) return;
-            //b.ApplyAction("OnOff_On");
-            b.Enabled = true;
-            b.ApplyAction("Adn.ActionLaunchMissile");
+            var guidance = MissileSelectionAction[selectionMode]?.Invoke();
+            if (guidance == null) return;
+
+            var beacon = SelectBlock(beaconBlocks, guidance, double.MaxValue, LessThan);
+            if (beacon != null) {
+                beacon.Enabled = true;
+                beacon.Radius = 50000;
+            }
+
+            guidance.Enabled = true;
+            guidance.ApplyAction("Adn.ActionLaunchMissile");
         }
         void TargetRandomBlockOnAll() {
-            gdblocks.ForEach(b => SetTargetRandomBlock(b, true));
+            guidanceBlocks.ForEach(b => SetTargetRandomBlock(b, true));
         }
         void TargetRandomBlockOffAll() {
-            gdblocks.ForEach(b => SetTargetRandomBlock(b, false));
+            guidanceBlocks.ForEach(b => SetTargetRandomBlock(b, false));
         }
         void SetTargetRandomBlock(IMyRadioAntenna b, bool random) {
-            //b.ApplyAction("OnOff_On");
             b.Enabled = true;
             b.SetValueBool("Adn.PropertyTargetRandomGridBlock", random);
-            //b.ApplyAction("OnOff_Off");
             b.Enabled = true;
         }
 
 
 
         IMyRadioAntenna SelectRandomMissile() {
-            var rndIndex = randomGenerator.Next(gdblocks.Count);
-            return gdblocks[rndIndex];
+            if (guidanceBlocks.Count == 0) return null;
+            var rndIndex = randomGenerator.Next(guidanceBlocks.Count);
+            return guidanceBlocks[rndIndex];
         }
-        IMyRadioAntenna SelectClosestMissile() => SelectMissile(double.MaxValue, LessThan);
-        IMyRadioAntenna SelectFurthestMissile() => SelectMissile(0d, GreaterThan);
+        IMyRadioAntenna SelectClosestMissile() => SelectBlock(guidanceBlocks, referenceBlock, double.MaxValue, LessThan);
+        IMyRadioAntenna SelectFurthestMissile() => SelectBlock(guidanceBlocks, referenceBlock, 0d, GreaterThan);
 
-        IMyRadioAntenna SelectMissile(double initalDist, Func<double, double, bool> compareFunc) {
-            IMyRadioAntenna selected = null;
+
+        static T SelectBlock<T>(List<T> blockList, IMyTerminalBlock referenceBlock, double initalDist, Func<double, double, bool> compareFunc) where T : IMyTerminalBlock {
+            T selected = default(T);
             var lastDist = initalDist;
             var refPosition = referenceBlock.GetPosition();
 
-            foreach (var b in gdblocks) {
+            foreach (var b in blockList) {
                 var currDist = (b.GetPosition() - refPosition).Length();
                 if (compareFunc(currDist, lastDist)) {
                     lastDist = currDist;
@@ -192,8 +233,9 @@ namespace IngameScript {
 
             return selected;
         }
-        bool GreaterThan(double a, double b) => a > b;
-        bool LessThan(double a, double b) => a < b;
+
+        static bool GreaterThan(double a, double b) => a > b;
+        static bool LessThan(double a, double b) => a < b;
 
     }
 }
