ecf679233af1780a2653486cc06e7242dd6fbf0d
SDragon42 <nathen.silver@gmail.com>
2019-06-30 16:31:44 -0600

    Added block report code

--- Block Report/Program.cs
+++ Block Report/Program.cs
@@ -19,12 +19,38 @@ using VRageMath;
 
 namespace IngameScript {
     partial class Program : MyGridProgram {
-        
+
         public Program() {
         }
 
 
         public void Main(string argument, UpdateType updateSource) {
+            var text = new StringBuilder();
+
+            text.Append($"Run at: {DateTime.Now:hh:mm:ss}\n");
+            text.Append("--------------------\n");
+
+            var allBlocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType(allBlocks);//, b => !(b is IMyThrust) && !(b is IMyShipConnector));
+
+            allBlocks
+                .Select(b => b.GetType().ToString())
+                .Distinct()
+                .Select(s => new {
+                    typeName = s,
+                    count = allBlocks.Where(b => b.GetType().ToString().Equals(s)).Count()
+                })
+                .Select(t => new {
+                    typeName = t.typeName.Substring(t.typeName.LastIndexOf('.')+1),
+                    t.count
+                })
+                .OrderBy(bt => bt.typeName)
+                .ToList()
+                .ForEach(bt => text.Append($"{bt.count,2:N0}  {bt.typeName}\n"))
+                ;
+
+            Me.CustomData = text.ToString();
+            Echo(Me.CustomData);
         }
     }
 }
