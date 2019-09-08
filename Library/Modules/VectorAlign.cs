85f8532eb5d5f29da2293d0a42b0e75085c57bb0
SDragon42 <nathen.silver@gmail.com>
2019-09-08 13:32:40 -0600

    Refactored VectorAlign class.

--- Library/Modules/VectorAlign.cs
+++ Library/Modules/VectorAlign.cs
@@ -27,8 +27,7 @@ namespace IngameScript {
         // Originally from: http://forums.keenswh.com/threads/aligning-ship-to-planet-gravity.7373513/#post-1286885461
         class VectorAlign {
 
-            const float MIN_ANGLE_RADIANS = 0.01f; // how tight to maintain aim Lower is tighter.
-
+            const float MinimumAngleRadians = 0.01f; // How tight to maintain aim. Smaller is tighter.
             readonly double ctrlCoeff;
 
             public VectorAlign(double controlCoeff = 0.8) {
@@ -37,64 +36,66 @@ namespace IngameScript {
 
             public bool AlignWithGravity(IMyShipController sc, Direction dir, IList<IMyGyro> gyroList, bool keepOverrideOn = false) {
                 if (sc == null) return false;
-                var grav = sc.GetNaturalGravity();
-                return AlignWithVector(grav, sc, dir, gyroList, keepOverrideOn);
+
+                var gravityVec = sc.GetNaturalGravity();
+                return AlignWithVector(gravityVec, sc, dir, gyroList, keepOverrideOn);
             }
 
-            public bool AlignWithVector(Vector3D vDirection, IMyShipController sc, Direction dir, IList<IMyGyro> gyroList, bool keepOverrideOn = false) {
+            public bool AlignWithVector(Vector3D targetVec, IMyShipController sc, Direction dir, IList<IMyGyro> gyroList, bool keepOverrideOn = false) {
                 if (sc == null) return false;
-                var bAligned = true;
 
-                Matrix or;
-                sc.Orientation.GetMatrix(out or);
+                Matrix orient;
+                sc.Orientation.GetMatrix(out orient);
 
-                var down = GetAlignmentVector(or, dir);
-                vDirection.Normalize();
+                var currentVec = GetDirectionVector(dir, orient);
+                currentVec.Normalize();
+                targetVec.Normalize();
 
+                var aligned = true;
                 for (var i = 0; i < gyroList.Count; ++i) {
-                    var g = gyroList[i];
-                    g.Orientation.GetMatrix(out or);
-                    var localDown = Vector3D.Transform(down, MatrixD.Transpose(or));
-                    var localGrav = Vector3D.Transform(vDirection, MatrixD.Transpose(g.WorldMatrix.GetOrientation()));
+                    var rotationVec = GetRotationVector(gyroList[i], currentVec, targetVec);
 
-                    // Since the gyro UI lies, we are not trying to control yaw,pitch,roll but rather we need a rotation vector (axis around which to rotate) 
-                    var rot = Vector3D.Cross(localDown, localGrav);
-                    var dot2 = Vector3D.Dot(localDown, localGrav);
-                    var ang = rot.Length();
-                    ang = Math.Atan2(ang, Math.Sqrt(Math.Max(0.0, 1.0 - ang * ang)));
-                    if (dot2 < 0) ang += Math.PI; // compensate for >+/-90
-                    if (ang < MIN_ANGLE_RADIANS) { // close enough 
-                        SetGyroOff(g, keepOverrideOn);
-                        continue;
-                    }
-
-                    var yawMax = g.GetMaximum<float>("Yaw");
-                    var ctrl_vel = yawMax * (ang / Math.PI) * ctrlCoeff;
-                    ctrl_vel = Math.Min(yawMax, ctrl_vel);
-                    ctrl_vel = Math.Max(0.01, ctrl_vel);
-                    rot.Normalize();
-                    rot *= ctrl_vel;
-
-                    SetGyro(g,
-                        (float)rot.GetDim(0),
-                        -(float)rot.GetDim(1),
-                        -(float)rot.GetDim(2),
-                        true);
-
-                    bAligned = false;
+                    aligned &= (rotationVec == Vector3D.Zero);
+                    SetGyro(gyroList[i],
+                        (float)rotationVec.GetDim(0),
+                        -(float)rotationVec.GetDim(1),
+                        -(float)rotationVec.GetDim(2),
+                        !aligned || keepOverrideOn);
                 }
-                return bAligned;
+                return aligned;
             }
 
-            public void gyrosOff(List<IMyGyro> gyroList) {
-                if (gyroList == null) return;
-                gyroList.ForEach(g => SetGyroOff(g, false));
+            private Vector3D GetRotationVector(IMyGyro g, Vector3D currentVec, Vector3D targetVec)
+            {
+                // Transform vectors
+                Matrix orient;
+                g.Orientation.GetMatrix(out orient);
+                currentVec = Vector3D.Transform(currentVec, MatrixD.Transpose(orient));
+                targetVec = Vector3D.Transform(targetVec, MatrixD.Transpose(g.WorldMatrix.GetOrientation()));
+
+                // Since the gyro UI lies, we are not trying to control yaw,pitch,roll but rather we need a rotation vector (axis around which to rotate) 
+                var rotationVector = Vector3D.Cross(currentVec, targetVec);
+                var dotVector = Vector3D.Dot(currentVec, targetVec);
+
+                var ang = rotationVector.Length();
+                ang = Math.Atan2(ang, Math.Sqrt(Math.Max(0.0, 1.0 - ang * ang)));
+                if (dotVector < 0) ang += Math.PI; // compensate for >+/-90
+
+                if (ang < MinimumAngleRadians) return Vector3D.Zero; // close enough 
+
+                var yawMax = g.GetMaximum<float>("Yaw");
+                var ctrl_vel = yawMax * (ang / Math.PI) * ctrlCoeff;
+                ctrl_vel = Math.Min(yawMax, ctrl_vel);
+                ctrl_vel = Math.Max(0.01, ctrl_vel);
+
+                rotationVector.Normalize();
+                rotationVector *= ctrl_vel;
+                return rotationVector;
             }
 
-            private void SetGyroOff(IMyGyro g, bool gyroOverride) {
-                SetGyro(g, 0F, 0F, 0F, gyroOverride);
-            }
-            private void SetGyro(IMyGyro g, float pitch, float yaw, float roll, bool gyroOverride) {
+            public static void SetGyrosOff(List<IMyGyro> gyroList) => gyroList?.ForEach(g => SetGyroOff(g, false));
+            public static void SetGyroOff(IMyGyro g, bool gyroOverride) => SetGyro(g, 0F, 0F, 0F, gyroOverride);
+            public static void SetGyro(IMyGyro g, float pitch, float yaw, float roll, bool gyroOverride) {
                 // Goes wonky if the properties are used.
                 g.SetValueFloat("Pitch", pitch);
                 g.SetValueFloat("Yaw", yaw);
@@ -102,14 +103,15 @@ namespace IngameScript {
                 g.GyroOverride = gyroOverride;
             }
 
-            Vector3D GetAlignmentVector(Matrix craftOrient, Direction dir) {
+            static Vector3D GetDirectionVector(Direction dir, Matrix orient)
+            {
                 switch (dir) {
-                    case Direction.Forward: return craftOrient.Forward;
-                    case Direction.Backward: return craftOrient.Backward;
-                    case Direction.Left: return craftOrient.Left;
-                    case Direction.Right: return craftOrient.Right;
-                    case Direction.Up: return craftOrient.Up;
-                    default: return craftOrient.Down;
+                    case Direction.Forward: return orient.Forward;
+                    case Direction.Backward: return orient.Backward;
+                    case Direction.Left: return orient.Left;
+                    case Direction.Right: return orient.Right;
+                    case Direction.Up: return orient.Up;
+                    default: return orient.Down;
                 }
             }
         }
