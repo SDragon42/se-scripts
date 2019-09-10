d8199be09e75f815a25ad3cb9f21c8a443201ef8
SDragon42 <nathen.silver@gmail.com>
2019-09-09 22:15:08 -0600

    Simplified the AutoDoorCloser module.

--- Library/Modules/AutoDoorCloser.cs
+++ Library/Modules/AutoDoorCloser.cs
@@ -18,60 +18,39 @@ using VRage.Game;
 using VRage;
 using VRageMath;
 
-namespace IngameScript
-{
-    partial class Program
-    {
+namespace IngameScript {
+    partial class Program {
         /// <summary>Automatically will close doors after a set amount of time
         /// </summary>
         /// <remarks>
         /// Whiplash's : Whip's Auto Door and Airlock Script
         /// http://steamcommunity.com/sharedfiles/filedetails/?id=416932930
         /// </remarks>
-        class AutoDoorCloser
-        {
+        class AutoDoorCloser {
             readonly Dictionary<IMyDoor, double> _openDoors = new Dictionary<IMyDoor, double>();
 
-            public AutoDoorCloser(double secondsToLeaveOpen = 4) { SecondsToLeaveOpen = secondsToLeaveOpen; }
-
-            public double SecondsToLeaveOpen { get; set; }
-
-            double _timeSeconds;
-
-            public void CloseOpenDoors(TimeSpan timeSinceLastCall, List<IMyTerminalBlock> doorList)
-            {
-                _timeSeconds = timeSinceLastCall.TotalSeconds;
-                doorList.ForEach(b => ProcessDoor(b as IMyDoor));
-            }
-            public void CloseOpenDoors(TimeSpan timeSinceLastCall, List<IMyDoor> doorList)
-            {
-                _timeSeconds = timeSinceLastCall.TotalSeconds;
-                doorList.ForEach(ProcessDoor);
-            }
-            public void CloseOpenDoors(double totalSeconds, List<IMyTerminalBlock> doorList)
-            {
-                _timeSeconds = totalSeconds;
-                doorList.ForEach(b => ProcessDoor(b as IMyDoor));
-            }
-            public void CloseOpenDoors(double totalSeconds, List<IMyDoor> doorList)
-            {
-                _timeSeconds = totalSeconds;
-                doorList.ForEach(ProcessDoor);
+            public AutoDoorCloser(double secondsToLeaveOpen = 4) {
+                _secondsToLeaveOpen = secondsToLeaveOpen > 0.0 ? secondsToLeaveOpen : 0.0;
             }
 
-            void ProcessDoor(IMyDoor door)
-            {
+            double _secondsToLeaveOpen;
+
+            public void CloseOpenDoors(IMyGridProgramRuntimeInfo runtime, List<IMyDoor> doorList) {
+                var timeSeconds = runtime.TimeSinceLastRun.TotalSeconds;
+                foreach (var door in doorList)
+                    ProcessDoor(door, timeSeconds);
+            }
+
+            void ProcessDoor(IMyDoor door, double timeSeconds) {
                 if (door == null) return;
-                if (_openDoors.ContainsKey(door))
-                {
-                    switch (door.Status)
-                    {
+                if (_openDoors.ContainsKey(door)) {
+                    switch (door.Status) {
                         case DoorStatus.Closed: _openDoors.Remove(door); break;
                         case DoorStatus.Closing: _openDoors.Remove(door); break;
                         case DoorStatus.Open:
                             double doorTime;
                             _openDoors.TryGetValue(door, out doorTime);
-                            doorTime -= _timeSeconds;
+                            doorTime -= timeSeconds;
                             _openDoors.Remove(door);
 
                             if (doorTime > 0.0)
@@ -80,11 +59,9 @@ namespace IngameScript
                                 door.CloseDoor();
                             break;
                     }
-                }
-                else
-                {
+                } else {
                     if (door.Status == DoorStatus.Open)
-                        _openDoors.Add(door, SecondsToLeaveOpen);
+                        _openDoors.Add(door, _secondsToLeaveOpen);
                 }
             }
 
