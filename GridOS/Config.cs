76913611c10dc3e53461cf6e00ee930a7a8a07a0
SDragon42 <nathen.silver@gmail.com>
2019-09-14 16:41:27 -0600

    Added base GridOS script. Added Auto Door Closing

---
+++ GridOS/Config.cs
@@ -0,0 +1,55 @@
+﻿// <mdk sortorder="100" />
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript {
+    partial class Program {
+        public class Config {
+            readonly MyIni ini = new MyIni();
+            int hash = 0;
+
+            public bool AutoDoorCloserEnabled { get; set; } = true;
+
+            public void Load(IMyTerminalBlock b, Program thiso) {
+                if (hash == b.CustomData.GetHashCode()) return;
+
+                ini.Clear();
+                ini.TryParse(b.CustomData);
+
+                thiso.blockReload_Time = ini.Add("Grid OS", "Block Reload Delay", thiso.blockReload_Time).ToDouble();
+
+                AutoDoorCloserEnabled = ini.Add("Auto Door Closer", "Enabled", AutoDoorCloserEnabled).ToBoolean();
+                thiso.autoDoorCloser.CloseDelay = ini.Add("Auto Door Closer", "Delay", thiso.autoDoorCloser.CloseDelay).ToDouble();
+
+                SaveConfig(b);
+            }
+
+            //public void Save(IMyTerminalBlock b) {
+            //    ini.Set(SEC_AutoDoorCloser, KEY_Enabled, AutoDoorCloserEnabled);
+
+            //    SaveConfig(b);
+            //}
+
+            void SaveConfig(IMyTerminalBlock me) {
+                me.CustomData = ini.ToString();
+                hash = me.CustomData.GetHashCode();
+            }
+        }
+    }
+}
---
+++ GridOS/Program.cs
@@ -0,0 +1,62 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript {
+    partial class Program : MyGridProgram {
+
+        
+
+
+        readonly RunningSymbol symbol = new RunningSymbol();
+        readonly AutoDoorCloser autoDoorCloser = new AutoDoorCloser();
+
+        readonly Config config = new Config();
+
+        readonly List<IMyDoor> autoDoors = new List<IMyDoor>();
+
+        double blockReload_Time = 10;
+        double blockReload_TimeElapsed = 0;
+
+        public Program() {
+            config.Load(Me, this);
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+        }
+
+        public void Save() {
+        }
+
+        public void Main(string argument, UpdateType updateSource) {
+            blockReload_TimeElapsed += Runtime.TimeSinceLastRun.TotalSeconds;
+            Echo($"Grid OS {symbol.GetSymbol(Runtime)}");
+            //Echo($"Block Reload in {Math.Truncate(blockReload_Time - blockReload_TimeElapsed) + 1:N0} seconds.");
+            config.Load(Me, this);
+            LoadBlocks();
+            if (config.AutoDoorCloserEnabled) autoDoorCloser.CloseOpenDoors(Runtime, autoDoors);
+        }
+
+
+        void LoadBlocks(bool forceLoad = false) {
+            if (!forceLoad && blockReload_TimeElapsed < blockReload_Time) return;
+
+            GridTerminalSystem.GetBlocksOfType(autoDoors);
+
+            blockReload_TimeElapsed = 0;
+        }
+    }
+}
