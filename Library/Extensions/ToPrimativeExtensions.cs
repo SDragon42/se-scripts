4c7ec02c964e5da2e6edb3fe4c14fce675d60384
SDragon42 <nathen.silver@gmail.com>
2020-07-13 19:00:24 -0600

    Added method comments

--- Library/Extensions/ToPrimativeExtensions.cs
+++ Library/Extensions/ToPrimativeExtensions.cs
@@ -19,30 +19,61 @@ using VRageMath;
 
 namespace IngameScript {
     static class ToPrimativeExtensions {
+        /// <summary>
+        /// Tries to convert the string value to an int.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="defValue"></param>
+        /// <returns></returns>
         public static int ToInt(this string text, int defValue = 0) {
             if (text == null) return defValue;
             int val;
             if (!int.TryParse(text, out val)) return defValue;
             return val;
         }
+        /// <summary>
+        /// Tries to convert the string value to a float.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="defValue"></param>
+        /// <returns></returns>
         public static float ToFloat(this string text, float defValue = 0f) {
             if (text == null) return defValue;
             float val;
             if (!float.TryParse(text, out val)) return defValue;
             return val;
         }
+        /// <summary>
+        /// Tries to convert the string value to a double.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="defValue"></param>
+        /// <returns></returns>
         public static double ToDouble(this string text, double defValue = 0.0) {
             if (text == null) return defValue;
             double val;
             if (!double.TryParse(text, out val)) return defValue;
             return val;
         }
+        /// <summary>
+        /// Tries to convert the string value to a boolean.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="defValue"></param>
+        /// <returns></returns>
         public static bool ToBoolean(this string text, bool defValue = false) {
             if (text == null) return defValue;
             bool val;
             if (!bool.TryParse(text, out val)) return defValue;
             return val;
         }
+        /// <summary>
+        /// Tries to convert the string value to an enum.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="text"></param>
+        /// <param name="defValue"></param>
+        /// <returns></returns>
         public static T ToEnum<T>(this string text, T defValue = default(T)) where T : struct {
             if (text == null) return defValue;
             T val;
