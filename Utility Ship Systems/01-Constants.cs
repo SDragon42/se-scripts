da0a3d8e53dcb1a9e277c6b20fced1256c5b95d5
SDragon42 <nathen.silver@gmail.com>
2024-08-28 20:23:30 -0600

    Upgraded to MDK2

--- Utility Ship Systems/01-Constants.cs
+++ Utility Ship Systems/01-Constants.cs
@@ -1,24 +1,27 @@
-﻿using Sandbox.Game.EntityComponents;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Sandbox.Game.EntityComponents;
 using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
 using SpaceEngineers.Game.ModAPI.Ingame;
-using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
-using System.Text;
-using System;
+using VRage;
 using VRage.Collections;
+using VRage.Game;
 using VRage.Game.Components;
 using VRage.Game.GUI.TextPanel;
-using VRage.Game.ModAPI.Ingame.Utilities;
 using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
 using VRage.Game.ObjectBuilders.Definitions;
-using VRage.Game;
-using VRage;
 using VRageMath;
 
-namespace IngameScript {
-    partial class Program {
+namespace IngameScript
+{
+    partial class Program
+    {
         const double BLOCK_RELOAD_TIME = 10;
         const float DEFAULT_SCREEN_WIDTH = 512f;
         const float DISPLAY_RANGE_FONT_SIZE = 3.5f;
